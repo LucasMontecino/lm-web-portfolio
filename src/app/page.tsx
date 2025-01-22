733cafe0e8e206f2eb4d45f81d3bcdf201ac63a5
Lucas Montecino <lucasmontecino95@gmail.com>
2025-01-22 18:36:27 -0300

    update - page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from '@/components/Hero/Hero';
 import Navbar from '@/components/Navbar/Navbar';
+import Work from '@/components/Work/Work';
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Work />
     </div>
   );
 }
