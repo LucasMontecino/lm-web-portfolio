b1b622e95a37bfa6766f51fd4fa36bf19a7311e1
Lucas Montecino <lucasmontecino95@gmail.com>
2025-01-21 21:58:15 -0300

    create - Hero component

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,47 @@
+import {
+  Box,
+  Container,
+  CssBaseline,
+  Typography,
+} from '@mui/material';
+import Image from 'next/image';
+
+export default function Hero() {
+  return (
+    <>
+      <CssBaseline />
+      <Container
+        sx={{
+          mt: '100px',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Box>
+          <Image
+            src={
+              'https://res.cloudinary.com/ds1jj2ef8/image/upload/v1737496436/lucas-img_axf58w.png'
+            }
+            alt="lm-img"
+            width={'220'}
+            height={'230'}
+            style={{
+              borderRadius: '50%',
+              padding: '12px',
+              border: '3px solid #f7dc6f',
+            }}
+          />
+        </Box>
+        <Box>
+          <Typography variant="h1">
+            I&apos;m Lucas Montecino.
+          </Typography>
+          <Typography variant="h2">
+            A passionate Frontend Developer
+          </Typography>
+        </Box>
+      </Container>
+    </>
+  );
+}
