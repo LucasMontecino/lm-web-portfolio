b58433fde3d512d84f4eaf4e16a16c9e27bc1b0d
Lucas Montecino <lucasmontecino95@gmail.com>
2025-01-21 21:57:58 -0300

    update - theme properties

--- src/theme.ts
+++ src/theme.ts
@@ -5,6 +5,20 @@ const theme = createTheme({
   typography: {
     fontFamily: 'var(--font-roboto)',
   },
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#3f50b5',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
 });
 
 export default theme;
