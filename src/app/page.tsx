abb72f7ee77fd83a5837e62c71ddde28e7e530d2
Lucas Montecino <lucasmontecino95@gmail.com>
2025-01-30 19:03:32 -0300

    update - pagetsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import ContactMe from '@/components/ContactMe/ContactMe';
+import Footer from '@/components/Footer/Footer';
 import Hero from '@/components/Hero/Hero';
 import Navbar from '@/components/Navbar/Navbar';
+import Resume from '@/components/Resume/Resume';
+import TechStack from '@/components/TechStack/TechStack';
 import Work from '@/components/Work/Work';
-import { Box } from '@mui/material';
+import { Box, Container } from '@mui/material';
 
 export default function Home() {
   return (
@@ -10,11 +14,33 @@ export default function Home() {
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
+        width: '100%',
       }}
     >
       <Navbar />
-      <Hero />
-      <Work />
+      <Container
+        component={'main'}
+        sx={{
+          width: '100%',
+          maxWidth: 'lg',
+          px: { xs: 2, sm: 4, md: 6 },
+        }}
+      >
+        <Hero />
+        <Box sx={{ my: 8 }}>
+          <Work />
+        </Box>
+        <Box sx={{ my: 8 }}>
+          <TechStack />
+        </Box>
+        <Box sx={{ my: 8 }}>
+          <Resume />
+        </Box>
+        <Box sx={{ my: 8 }}>
+          <ContactMe />
+        </Box>
+      </Container>
+      <Footer />
     </Box>
   );
 }
